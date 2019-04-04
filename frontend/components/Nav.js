0ae5d12bca6ad09c6b65cd11288488dc645ed3ce
jmpainter <jpainter4@gmail.com>
2019-04-04 09:42:22 -0700

    styled Nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,17 +1,38 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-const NavStyles = styled.ul`
-  margin: 0;
-  padding: 0;
+const NavStyles = styled.div`
+  height: 45px;
+  border: 1px solid rgb(225, 225, 225);
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: #bf9764;
+    font-size: 14px;
+    text-transform: uppercase;
+    line-height: 45px;
+
+    height: 45px;
+    display: inline-block;
+    padding: 0 5px;
+    border-left: 1px solid #e1e1e1;
+  }
+
+  a:last-child {
+    border-right: 1px solid #e1e1e1;
+  }
 `;
 
 const Nav = () => (
   <NavStyles>
-    <Link href="/login">
+    <Link href="/find-a-recipe">
       <a>Find a Recipe</a>
     </Link>
-    <Link href="/sign-up">
+    <Link href="/add-a-recipe">
       <a>Add a Recipe</a>
     </Link>
   </NavStyles>
